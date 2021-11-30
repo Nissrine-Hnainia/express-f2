65ad0c91223a591dff7167dc911a40dfb98601cb
Nissrine-Hnainia <hnainianesrine@gmail.com>
2021-11-30 19:39:33 +0100

    middlewares

--- server.js
+++ server.js
@@ -1,43 +1,34 @@
-//1-require/import express 
-const express = require('express')
+//1-require/import express
+const express = require("express");
 
 //2- init the application
-const app = express()
-
+const app = express();
 
 //5-routing
-app.get('/', (req, res) => {
-    res.send('<h2> this is the home page </h2>')
-    console.log(req.method)
-    console.log(req.url)
-})
+app.get("/", (req, res) => {
+    res.send("<h2> this is the home page </h2>");
+    console.log(req.method);
+    console.log(req.url);
+});
 
-app.get('/about', (req, res) => {
-    res.send('<h2> this is the about page </h2>')
-})
+app.get("/about", (req, res) => {
+    res.send("<h2> this is the about page </h2>");
+});
 
-app.get('/products/:productId', (req, res) => {
-    const {productId} = req.params
-    res.send(`<h2> this is the products page and the product id is : ${productId} </h2>`)
+app.get("/products/:productId", (req, res) => {
+    //to understand the request object params
+    const { productId } = req.params;
+    res.send(
+        `<h2> this is the products page and the product id is : ${productId} </h2>`
+    );
     // res.send("<h2> this is the products page and the product id is :" +productId+ " </h2>")
-})
-
-app.get("*", (req, res) => {
-    res.send("<h2> 404 not found </h2>")
-})
-
+});
 
 //6-middleware
 // function first(req, res, next){
 //     console.log('I am the first middleware')
 //     next()
 // }
-
-
-
-
-
-
 
 // function second(req, res, next){
 //     console.log('I am the second middleware')
@@ -52,11 +43,14 @@ app.get("*", (req, res) => {
 //     console.log('I am the finishing point')
 // })
 
+app.get("*", (req, res) => {
+    res.send("<h2> 404 not found </h2>");
+});
 
-
-//3-create the port 
-const PORT = 5005
+//3-create the port
+const PORT = 5005;
 
 //4-running the app
-app.listen(PORT, err => 
-    err? console.log(err) : console.log(`The app is listening on port ${PORT}`))
+app.listen(PORT, (err) =>
+    err ? console.log(err) : console.log(`The app is listening on port ${PORT}`)
+);
